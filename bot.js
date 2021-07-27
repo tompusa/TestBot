cb5f945641a4dd5d2241bc6b88b04c1ddb366a44
tompusa <87921101+tompusa@users.noreply.github.com>
2021-07-27 13:48:05 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ const Language = require('./language');
 const Lang = Language.getString('updater');
 
 // Sql
-const WhatsAsenaDB = config.DATABASE.define('TestBot', {
+const WhatsAsenaDB = config.DATABASE.define('WhatsAsenaDuplicated', {
     info: {
       type: DataTypes.STRING,
       allowNull: false
